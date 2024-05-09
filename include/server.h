984c4d25700bb7b4bfcaeb6f62fdc82ccaa218f8
Xing Zhikai <haoyunbaobei1209@gmail.com>
2024-05-09 13:36:17 +0800

    Update ans
    pass all unit test

--- include/server.h
+++ include/server.h
@@ -1,5 +1,27 @@
+#include <map>
+#include <memory>
+#include <string>
+#include <vector>
+
 #ifndef SERVER_H
 #define SERVER_H
+#include "client.h"
 
+class Client;  // forward declaration
+extern std::vector<std::string> pending_trxs;
+class Server {
+ public:
+  Server();
+  std::shared_ptr<Client> add_client(std::string id);
+  std::shared_ptr<Client> get_client(std::string id) const;
+  double get_wallet(std::string id) const;
+  static bool parse_trx(std::string trx, std::string &sender,
+                        std::string &receiver, double &value);
+  bool add_pending_trx(std::string trx, std::string signature) const;
+  size_t mine();
+  friend void show_wallets(const Server &server);
 
-#endif //SERVER_H
+ private:
+  std::map<std::shared_ptr<Client>, double> clients;
+};
+#endif  // SERVER_H
